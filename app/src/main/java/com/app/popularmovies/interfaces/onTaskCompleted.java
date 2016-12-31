d6b87873e1a2e73beb8859d3d3c467e4eb8187ff
hiteshsantwani <hiteshsantwani35@gmail.com>
2016-12-31 18:18:23 +0530

    final commit

---
+++ app/src/main/java/com/app/popularmovies/interfaces/onTaskCompleted.java
@@ -0,0 +1,10 @@
+package com.app.popularmovies.interfaces;
+
+
+/**
+ * Created by hitesh on 26-Dec-15.
+ */
+public interface onTaskCompleted {
+
+    public void onTaskCompleted(Object object);
+}
---
+++ app/src/main/java/com/app/popularmovies/util/Constants.java
@@ -0,0 +1,28 @@
+package com.app.popularmovies.util;
+
+/**
+ * Created by hitesh on 26-Dec-15.
+ */
+public interface Constants {
+
+    public String REQUEST_BASE_URL = "http://api.themoviedb.org/3/discover/movie";
+    public String TRAILER_REVIEWS_BASE_URL = "http://api.themoviedb.org/3/movie";
+    public String YOU_TUBE_IMAGE_BASE_URL = "http://i3.ytimg.com/vi/";
+    public String YOU_TUBE_BASE_URL = "http://www.youtube.com/watch?v=";
+    public String API_KEY = "YOUR_API_KEY";
+
+    public String IMAGE_PATH_BASE_URL = "http://image.tmdb.org/t/p/w185/";
+    public String BACKDROP_IMAGE_PATH_BASE_URL = "http://image.tmdb.org/t/p/w342/";
+    public int MOST_POPULAR_REQUEST = 1;
+    public int TOP_RATED_REQUEST = 2;
+    public int VIDEO_TRAILER_REQUEST = 3;
+    public int MOVIE_REVIEWS_REQUEST = 4;
+
+    public String FRAGMENT_TYPE_FAVORITE = "FAVORITE";
+
+
+
+//    public String REQUEST_TYPE = "POPULAR MOVIE";
+//    public String PAGE_NUMBER = "PAGE_NUMBER";
+
+}
